6f614e31bf84de8ef35bc34b893f1de8dd4d2c61
Riku Soikkeli <riku.soikkeli@hotmail.fi>
2021-03-30 20:19:03 +0300

    db alustavasti valmis ja muutama test case

--- network/models.py
+++ network/models.py
@@ -1,6 +1,91 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+import datetime
 
 
 class User(AbstractUser):
-    pass
+    photo = models.ImageField(upload_to="photos", blank=True)
+
+    def __str__(self):
+        return f"{self.username}"
+
+
+class Follow(models.Model):
+    follower = models.ForeignKey(User, on_delete=models.CASCADE, blank=False, related_name="follower")
+    followed = models.ForeignKey(User, on_delete=models.CASCADE, blank=False, related_name="followed")
+
+    class Meta:
+        constraints = [
+            # constraint: follower != followed
+            models.CheckConstraint(
+                    check=~models.Q(follower=models.F('followed')), 
+                    name="follower_followed_must_be_unequal")
+        ]
+
+    def __str__(self):
+        return f"{self.follower} -> {self.followed}"
+
+    def isValidFollow(self):
+        if self.follower != self.followed and isinstance(self.follower, User) and isinstance(self.followed, User):
+            return True
+        else:
+            return False
+
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=False)
+    content = models.CharField(max_length=280, blank=False)
+    time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        if len(self.content) > 20:
+            return f"{self.user}: {self.content[0:19]}..."
+        else:
+            return f"{self.user}: {self.content}"
+
+    def isValidPost(self):
+        if isinstance(self.user, User) and isinstance(self.content, str) and isinstance(self.time, datetime.datetime):
+            return True
+        else:
+            return False
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=False)
+    content = models.CharField(max_length=280, blank=False)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, blank=False)
+    time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        if len(self.content) > 20:
+            return f"{self.user}: {self.content[0:19]}..."
+        else:
+            return f"{self.user}: {self.content}"
+
+    def isValidComment(self):
+        if (isinstance(self.user, User) and isinstance(self.content, str) and isinstance(self.post, Post) and 
+        isinstance(self.time, datetime.datetime)):
+            return True
+        else:
+            return False
+
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=False)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, blank=False)
+
+    class Meta:
+        constraints = [
+            # sallitaan käyttäjältä vain yksi tykkäys per postaus
+            models.UniqueConstraint(fields=["user", "post"], name="user_post_must_be_unique"),
+        ]
+
+    def __str__(self):
+        return f"{self.user} -> post({self.post.id})"
+
+    def isValidLike(self):
+        if isinstance(self.user, User) and isinstance(self.post, Post):
+            return True
+        else:
+            return False
+
--- network/tests.py
+++ network/tests.py
@@ -1,3 +1,64 @@
 from django.test import TestCase
+from .models import User, Follow, Post, Comment, Like
+import datetime
 
 # Create your tests here.
+class NetworkTestCase(TestCase):
+
+    def setUp(self):
+
+        #users
+        u1 = User.objects.create(username="Harry")
+        u2 = User.objects.create(username="Ron")
+        u3 = User.objects.create(username="Hermione")
+        #follows
+        f1 = Follow.objects.create(follower=u1, followed=u2)
+        f2 = Follow.objects.create(follower=u2, followed=u1)
+        #posts
+        p1 = Post.objects.create(user=u1, content="Hello! This is my first social media post!")
+        p2 = Post.objects.create(user=u1, content="Hello! This is my second social media post!")
+        p3 = Post.objects.create(user=u2, content="Hello!")
+        #comments
+        c1 = Comment.objects.create(user=u1, content="I'm commenting my own post!", post=p1)
+        c2 = Comment.objects.create(user=u2, content="I'm commenting my friend's post!", post=p1)
+        #likes
+        l1 = Like.objects.create(user=u1, post=p1)
+        l2 = Like.objects.create(user=u2, post=p3)
+
+
+    def testUsersCount(self):
+        all_users_count = len(User.objects.all())
+        self.assertEqual(all_users_count, 3)
+
+    def testFollowCount(self):
+        all_follows_count = len(Follow.objects.all())
+        self.assertEqual(all_follows_count, 2)
+
+    def testPostsCount(self):
+        all_posts_count = len(Post.objects.all())
+        self.assertEqual(all_posts_count, 3)
+
+    def testCommentsCount(self):
+        all_comments_count = len(Comment.objects.all())
+        self.assertEqual(all_comments_count, 2)
+
+    def testLikesCount(self):
+        all_likes_count = len(Like.objects.all())
+        self.assertEqual(all_likes_count, 2)
+
+    def testValidFollow(self):
+        follow = Follow.objects.all()[0]
+        self.assertTrue(follow.isValidFollow())
+
+    def testValidPost(self):
+        post = Post.objects.all()[0]
+        self.assertTrue(post.isValidPost())
+
+    def testValidComment(self):
+        comment = Comment.objects.all()[0]
+        self.assertTrue(comment.isValidComment())
+        
+    def testValidLike(self):
+        like = Like.objects.all()[0]
+        self.assertTrue(like.isValidLike())
+
