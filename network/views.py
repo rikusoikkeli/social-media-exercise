6abad5dc300b344f94fe73d12c67250ca42ef7f7
Riku Soikkeli <riku.soikkeli@hotmail.fi>
2021-04-02 18:53:29 +0300

    taputeltu vähän css

--- network/views.py
+++ network/views.py
@@ -6,24 +6,51 @@ from django.urls import reverse
 from django.core.paginator import Paginator
 from django.http import JsonResponse
 import json
+from django import forms
 
 from .models import User, Follow, Post, Comment, Like
 
 
+class PostCommentForm(forms.Form):
+    content = forms.CharField(label=False)
+    post_id = forms.CharField(widget=forms.HiddenInput())
+    page = forms.CharField(widget=forms.HiddenInput())
+
+
 def index(request):
-    posts = Post.objects.all()
-    # Paginator params: iterable, objects per page
-    posts_paginated = Paginator(posts, 3)
-    # get method params: name from query string, default value if not found
-    page_num = request.GET.get("page", 1)
-    page = posts_paginated.page(page_num)
+    if request.method == "POST":
+        form = PostCommentForm(request.POST)
+        if form.is_valid():
+            user = request.user
+            post_id = form.cleaned_data["post_id"]
+            content = form.cleaned_data["content"]
+            page_number = form.cleaned_data["page"]
+            post = Post.objects.get(pk=post_id)
 
-    comments = Comment.objects.all()
+            new_comment = Comment(user=user, content=content, post=post)
+            new_comment.save()
 
-    return render(request, "network/index.html", {
-        "page": page,
-        "comments": comments
-    })
+            # Tehdään tämä, jotta palataan samalle Paginator-sivulle, jolla oltiin aiemmin.
+            url = reverse("index")+"?page="+str(page_number)
+            return HttpResponseRedirect(url)
+        else:
+            return HttpResponse("Form is not valid!")
+    else:
+        posts = Post.objects.all().order_by('-id')
+        # Paginator params: iterable, objects per page
+        posts_paginated = Paginator(posts, 3)
+        # get method params: name from query string, default value if not found
+        page_num = request.GET.get("page", 1)
+        page = posts_paginated.page(page_num)
+
+        comments = Comment.objects.all()
+        form = PostCommentForm()
+
+        return render(request, "network/index.html", {
+            "page": page,
+            "comments": comments,
+            "form": form
+        })
 
 
 def login_view(request):
@@ -89,11 +116,6 @@ def getComments(request, post_id):
 
     # Return comments
     if request.method == "GET":
-        #all_comments_dict = {}
-        #index = 0
-        #for comment in comments:
-        #    all_comments_dict[index] = comment.serialize()
-        #    index += 1
-        #return JsonResponse(all_comments_dict)
+        # Miksi tämä toimii?
         return JsonResponse([comment.serialize() for comment in comments], safe=False)
 
